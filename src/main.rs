281c98ec04cf622264b326a8987c6107a8f883e9
Rodolfo Udo Labsch <rodolfoul@gmail.com>
2024-08-16 22:37:54 -0300

    Start first project version

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use check_elevation::is_elevated;
+
+
+use std::process::Command;
+use std::{str, thread};
+use regex::Regex;
+use std::env;
+use std::time::Duration;
+
+fn main() {
+	let args: Vec<_> = env::args().collect();
+	let mut dry_run = if args.len() >= 2 && args[1].eq("-n") {
+		true
+	} else {
+		false
+	};
+
+	if !is_elevated().unwrap() {
+		println!("Not elevated, using dry run");
+		dry_run = true;
+	}
+	let user_listing = get_non_current_users();
+
+	if user_listing.is_empty() {
+		println!("No users to log out")
+
+	} else {
+		println!("Logging out the following users:");
+		for (user_name, id) in user_listing {
+			println!("id:{} - {}", id, user_name);
+			if !dry_run {
+				log_user_out(id);
+			}
+		}
+	}
+	println!("Exiting...");
+	thread::sleep(Duration::from_millis(4000));
+}
+
+fn log_user_out(id: i32) {
+	let result = Command::new("logoff")
+		.arg(id.to_string())
+		.output()
+		.expect("Could not log user off");
+
+	assert!(result.status.success());
+}
+
+fn get_non_current_users() -> Vec<(String, i32)> {
+	let process_result = Command::new("query")
+		.arg("session")
+		.output()
+		.expect("failed to execute process");
+
+	let other_user_sessions_reg = Regex::new(r"(?m)^[^>].*?((\S+)\s+(\d+)\s+)").unwrap();
+	let cmd_output = str::from_utf8(&process_result.stdout).unwrap();
+
+	let user_listing = other_user_sessions_reg.captures_iter(&cmd_output)
+		.map(|m| {
+			return (m.get(2).unwrap().as_str().to_string(), m.get(3).unwrap().as_str().parse::<i32>().unwrap());
+		})
+		.filter(|s| s.1 != 0)
+		.collect::<Vec<(String, i32)>>();
+
+	user_listing
+}
